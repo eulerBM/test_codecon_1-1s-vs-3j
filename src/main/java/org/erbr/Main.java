97bc091ca71db6b4be3e4d5f77678691f26d4970
euler <eullerborgesdamotta155@gmail.com>
2025-05-13 16:53:53 -0300

    get json done

--- src/main/java/org/erbr/Main.java
+++ src/main/java/org/erbr/Main.java
@@ -1,21 +1,25 @@
 package org.erbr;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.erbr.json.Pessoa;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         ObjectMapper mapper = new ObjectMapper();
 
         try {
-            Usuario usuario = mapper.readValue(new File("usuario.json"), Usuario.class);
-            System.out.println("Nome: " + usuario.getNome());
-            System.out.println("Idade: " + usuario.getIdade());
+
+            List<Pessoa> usuarios = mapper.readValue(new File("usuarios_1000.json"), mapper.getTypeFactory().constructCollectionType(List.class, Pessoa.class));
+
+            System.out.println(usuarios);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
